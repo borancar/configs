e570d6f2bea1541dfac10ba93ee4f3966e92cfc8
Boran Car <boran.car@gmail.com>
2025-09-29 23:21:28 +0100

    Add tofu-ls

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -5,8 +5,17 @@ local servers = {
   "gopls",
   "html",
   "lua-language-server",
+  "tofu-ls",
   "typescript-language-server",
 }
+
+vim.lsp.config["tofu-ls"] = {
+  cmd = { "tofu-ls", "serve" },
+  filetypes = { "terraform", "hcl", "tf", "tfvars", "tofu" },
+  root_markers = { ".git", ".terraform", "tofu.hcl" },
+  settings = {},
+}
+
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
